9c0bf73fc2ab4580493c23ee9f72b666d107fc1c
Enrico Thies <enrico.thies@gmail.com>
2014-01-29 22:28:04 +0100

    add bootstrap3

---
+++ ToaLayoutBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Toa\Bundle\LayoutBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * ToaLayoutBundle
+ *
+ * @author Enrico Thies <enrico.thies@gmail.com>
+ */
+class ToaLayoutBundle extends Bundle
+{
+}
